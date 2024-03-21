690dd0e38bb93dc4808bb6dc74d7fe1004ce81ff
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-03-21 22:16:57 +0300

    footer upper part

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 
+import imgg from '../assets/softnet.jpg'
+
 function Footer() {
   return (
-    <div>
-      <div className="">
+    <div className='bg-[#E9E0FF] m-10 max-sm:mx-3 sm:max-lg:mx-4'>
+      <div className="flex pt-10 gap-5 justify-center items-center px-2 max-md:gap-2 ">
+            <div className=" h-[2.2px] bg-[#755BB4] w-[42%]"></div>
+            <img src={imgg} className='w-28 h-20'/>
+            <div className=" h-[2.2px] bg-[#755BB4] w-[42%]"></div>
 
       </div>
       <div></div>
