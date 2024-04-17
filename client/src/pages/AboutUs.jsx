be34de606c4f05761c08e36bddc0dd19935bedf2
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-04-17 22:15:53 +0300

    make it responsive

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -10,12 +10,12 @@ function AboutUs() {
         <h1 className="text-2xl text-center text-white pt-[130px] font-semibold"> About Us</h1>       
       </div>
       
-      <div className="flex gap-10 justify-center pt-10">
-        <img src={about} className="w-[530px]"/>
-        <div className="flex flex-col gap-3 ">
+      <div className="flex gap-10 justify-center pt-10 max-ml:flex-col items-center">
+        <img src={about} className="w-[40%] max-sm:w-[80%] sm:max-ml:w-[60%] ml:max-lg:w-[50%]"/>
+        <div className="flex flex-col gap-3 px-2 w-[40%] max-sm:w-[90%] sm:max-ml:w-[85%] ml:max-lg:w-[50%]">
           <h1 className="text-2xl text-[#7F67BB]">Who We Are</h1>
-          <div className="h-[2px] w-[400px] bg-[#7F67BB]"></div>
-          <p className=" w-[500px] text-xl">
+          <div className="h-[2px] w-[400px] bg-[#7F67BB] max-xss:w-[300px] xss:max-lg:w-[350px]"></div>
+          <p className="  text-xl max-sm:text-lg ">
               Softnet is a technology firm specializing in comprehensive technology training,
               tailored software development, and end-to-end technology infrastructure solutions. 
               With over 20 years of experience in delivering technology trainings in Ethiopia, 
