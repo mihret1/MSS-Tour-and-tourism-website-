4df565a12f8426edb779abec20124271d7fa2700
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-04-17 17:06:06 +0300

    add map location to contact page

--- client/src/pages/ContactsUs.jsx
+++ client/src/pages/ContactsUs.jsx
@@ -19,7 +19,7 @@ function ContactsUs() {
     <div>
       <div className="bg-gradient-to-br from-[#755BB4] via-[#937DC7] via-49% to-[#846DBC] via-[#BEAFE3] via-77% h-[300px]">
         <NavBar />
-        <h1 className="text-2xl text-center text-white pt-[140px]"> Contact Us</h1>       
+        <h1 className="text-2xl text-center text-white pt-[130px]"> Contact Us</h1>       
       </div>
       
       <div className=" flex flex-col items-center gap-10 pt-7">
@@ -96,14 +96,27 @@ function ContactsUs() {
                 <div className="flex flex-col">
                    <span className="text-lg font-serif	">Your Comment</span>
                    <textarea 
-                   className="outline-none border-2 w-[350px] bg-[#D9D9D9] p-1 h-28" 
+                   className="outline-none border-2 w-[350px] bg-[#D9D9D9] p-1 h-24" 
                    value={comment} type="text" onChange={(e)=>setComment(e.target.value)} />
                 </div>
+                <button className="px-5 py-2  rounded-lg bg-gradient-to-r from-[#6C4277] to-[#ad99d9] text-white text-xl">Submit</button>
+                
+
 
 
               </div>
           </div> 
-       
+        
+          <div className="w-[100%] flex items-center justify-center">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.6185171121315!2d38.74380621072438!3d9.007205089292171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85e1f44917bd%3A0x6005a0aec6bbd54c!2sSoftnet%20Solutions%20-%20Ethiopia%20(Headquarters)!5e0!3m2!1sen!2sus!4v1713361627067!5m2!1sen!2sus" 
+             
+             height="330" 
+             style={{ border:0, }} 
+             allowfullscreen="" 
+             className="w-[95%]"
+            //  loading="lazy" 
+             referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
        
         </div>
     </div>
