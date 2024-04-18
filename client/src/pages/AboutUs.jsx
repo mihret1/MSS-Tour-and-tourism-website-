4c80f0cf19d50950f9bad2ae52d7daa0b4d0d035
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-04-18 09:10:24 +0300

    done 'what we do' part

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,16 +1,18 @@
 import NavBar from "../components/NavBar"
 import about from '../assets/about/abt.webp'
+import checkbox from '../assets/about/checkbox.png'
 
 
 function AboutUs() {
   return (
     <div>
+
       <div className="bg-gradient-to-br from-[#755BB4] via-[#937DC7] via-49% to-[#846DBC] via-[#BEAFE3] via-77% h-[300px]">
         <NavBar />
         <h1 className="text-2xl text-center text-white pt-[130px] font-semibold"> About Us</h1>       
       </div>
       
-      <div className="flex gap-10 justify-center pt-10 max-ml:flex-col items-center">
+       <div className="flex gap-10 justify-center pt-10 max-ml:flex-col items-center">
         <img src={about} className="w-[40%] max-sm:w-[80%] sm:max-ml:w-[60%] ml:max-lg:w-[50%]"/>
         <div className="flex flex-col gap-3 px-2 w-[40%] max-sm:w-[90%] sm:max-ml:w-[85%] ml:max-lg:w-[50%]">
           <h1 className="text-2xl text-[#7F67BB]">Who We Are</h1>
@@ -24,13 +26,13 @@ function AboutUs() {
               individuals at all stages of their learning journey, from eager beginners to seasoned experts. 
           </p>
         </div>
-      </div>
+       </div>
 
-      <div className="flex gap-10 justify-center pt-28 max-ml:flex-col items-center">
+       <div className="flex gap-10 justify-center pt-28 max-ml:flex-col items-center">
         <div className="flex flex-col items-center gap-3 px-1 w-[40%] max-sm:w-[90%] sm:max-ml:w-[85%] ml:max-lg:w-[50%]">
           <h1 className="text-2xl text-[#7F67BB]">Our Mission</h1>
           <div className="h-[2px] w-[400px] bg-[#7F67BB] max-xss:w-[300px] xss:max-lg:w-[350px]"></div>
-          <p className="  text-xl max-sm:text-lg ">
+          <p className="  text-xl max-sm:text-lg text-center ">
                Our mission is to provide creative and innovative technology solutions that effectively address the challenges 
              of our community, while also ensuring that these solutions are scalable and globally applicable. 
           </p>
@@ -38,14 +40,42 @@ function AboutUs() {
         <div className="flex flex-col items-center gap-3 px-1 w-[40%] max-sm:w-[90%] sm:max-ml:w-[85%] ml:max-lg:w-[50%]">
           <h1 className="text-2xl text-[#7F67BB]">Our Vision</h1>
           <div className="h-[2px] w-[400px] bg-[#7F67BB] max-xss:w-[300px] xss:max-lg:w-[350px]"></div>
-          <p className="  text-xl max-sm:text-lg ">
+          <p className="  text-xl max-sm:text-lg text-center">
           Our vision is to be an exceptional and sustainable Ethiopian company that delivers 
           innovative technology solutions that have a significant positive impact on people`s lives.
           </p>
         </div>
-      </div>
+       </div>
 
-      
+      <div className="flex flex-col items-center py-14 gap-3 m-14 bg-gradient-to-br from-[#7f64bd] to-[#D9D9D9]">
+        <h1 className=" text-2xl font-semibold">What We Do</h1>
+        <p className=" text-center text-lg w-[70%] font-semibold" >
+        Softnet is a technology firm specializing in comprehensive technology training, 
+        tailored software development, and end-to-end technology infrastructure solutions.
+        </p>
+       
+        <div className="flex justify-start gap-20 pt-4  ">
+
+          <div className="flex flex-col gap-3">
+            <div className="abt"> <img src={checkbox} className="w-8"/> Software Development</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Information Technology outsourcing (ITO)</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Technology Trainings</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Infrastructure supply and Deployment</div>
+
+          </div>
+          
+          <div className="flex flex-col gap-3">
+            <div className="abt"> <img src={checkbox} className="w-8"/> Software Development</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Information Technology outsourcing (ITO)</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Technology Trainings</div>
+            <div className="abt"> <img src={checkbox} className="w-8"/> Infrastructure supply and Deployment</div>
+
+          </div>
+        
+        </div>
+
+
+      </div>
 
 
     </div>
