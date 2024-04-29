7c3f2c1c87e857b725253b9593ca4bbd6b54e7bd
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-04-29 21:51:38 +0300

    finish the portfolio page

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -7,8 +7,8 @@ import p5 from '../assets/portfolio/p5.jpg'
 import p6 from '../assets/portfolio/p6.jpg'
 import p7 from '../assets/portfolio/p7.jpg'
 
-
-
+import p8 from '../assets/p/p12.jpg'
+import p9 from '../assets/h4.jpg'
 
 
 function Portfolio() {
@@ -115,6 +115,45 @@ function Portfolio() {
         
      </div>
 
+      <h1 className="text-2xl font-bold text-center pt-24">Some of Our Projects and Case Studies</h1>
+      <div className="flex px-14 max-ml:px-5 gap-6 pt-10 max-md:flex-col ">        
+        <div className=" flex flex-col">
+          <h1 className="text-xl font-semibold font-serif pb-3 pt-5">Project 1: Streamlining E-Commerce Operations </h1>
+          <p className="text-xl pr-10 max-ml:pr-1">
+             In this project, we optimized the client`s` e-commerce platform, resulting in a 30% increase in sales and improved user experience.
+             software solutions that drive success. Our portfolio showcases a range of projects that
+             highlight our expertise
+          </p>
+        </div>
+        <img src={p2} className=" w-[40%] max-md:w-[90%] "/>      
+      </div>
+
+
+      <div className="flex px-14 max-ml:px-5 gap-6 pt-24 max-md:flex-col">
+        
+        <img src={p8} className=" w-[38%] max-md:w-[80%] h-[220px]"/>
+        <div className=" flex flex-col">
+          <h1 className="text-2xl font-semibold font-serif pb-3 pt-5">Project 3: food delivery mobile application</h1>
+          <p className="text-xl pr-10 max-ml:pr-1">
+            In this project, we optimized the client`s 
+            e-commerce platform, resulting in a 30% increase in sales and improved user experience.
+          </p>
+        </div>
+      
+      </div>
+
+
+      <div className="flex px-14 max-ml:px-5 gap-3 pt-24 max-md:flex-col ">        
+        <div className=" flex flex-col">
+          <h1 className="text-xl font-semibold font-serif pb-3 pt-16">Project 4: Blockchain Integration for Supply Chain</h1>
+          <p className="text-xl pr-10 max-ml:pr-1">
+          We successfully implemented blockchain technology to enhance 
+          transparency and traceability in our client`s supply chain, reducing 
+          inefficiencies and minimizing errors.
+          </p>
+        </div>
+        <img src={p9} className=" w-[50%] max-md:w-[90%]  "/>      
+      </div>
 
     </div>
   )
