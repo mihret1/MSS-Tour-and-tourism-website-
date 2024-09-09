2cfd3f23fb272686bc9e74b710f9215b1ec998d4
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-09-09 13:53:59 +0300

    finish blog detail page

--- client/src/pages/BlogDetail.jsx
+++ client/src/pages/BlogDetail.jsx
@@ -22,8 +22,19 @@ const BlogDetail=()=>{
               <img src={props.img} className="h-24 w-1/4 rounded-md"/>
               <div className="flex flex-col justify-items-center  w-3/4">
                 <time className="text-lg font-serif ">{props.datee}</time>
-                <p className="text-xl font-semibold">{props.desc}</p>
+                <p className="text-xl font-semibold max-xss:text-lg">{props.desc}</p>
               </div>
+            </div>
+        )
+    }
+
+      const  Carde2=(props)=>{
+        return(
+            
+            <div className="flex flex-col shadow-md shadow-slate-600 w-1/3 max-sm:w-[80%] pb-4  h-[320px]">
+                <a href="/"><img src={props.img} className="h-44 w-[100%]"/></a>
+                <time className="text-lg font-serif px-1">{props.datee}</time>
+                <h1 className="text-lg font-semibold pl-1 hover:text-[#755BB4] "><a href="/">{props.title}</a></h1>
             </div>
         )
     }
@@ -31,12 +42,12 @@ const BlogDetail=()=>{
         <div>
            <div className="bg-gradient-to-tr from-[#755BB4] via-[#937DC7] via-49% to-[#846DBC] via-[#BEAFE3] via-77% h-[300px]">
             <NavBar />
-        <h1 className="text-center text-white text-xl font-semibold pt-32"> Latest News <ArrowForwardIosIcon /> Technology for all
-        </h1>
-        </div>
-           <div className="flex p-20 max-md:px-5 gap-10  max-lg:flex-col"> 
-             <div className=" w-3/5 flex flex-col gap-2 max-lg:w-[100%]">
-                     <img src={img1} className="w-[100%] h-[440px]"/>
+            <h1 className="text-center text-white text-xl font-semibold pt-32"> Latest News <ArrowForwardIosIcon /> Technology for all
+            </h1>
+           </div>
+           <div className="flex p-16 max-md:px-5 gap-10  max-lg:flex-col"> 
+             <div className=" w-3/5 flex flex-col gap-2 max-lg:w-[95%] border-2 p-2">
+                     <img src={img1} className="w-[100%] h-[440px] max-sm:h-[250px]"/>
                     <div className="flex items-center gap-10 pl-5 pt-2">
                         <time className='text-lg font-serif '><CalendarMonthIcon sx={{ color:'#755BB4' }} /> mar, 9/2023</time>
                        <span className="font-serif"> <CommentIcon  sx={{ color:'#755BB4' }} />
@@ -83,9 +94,9 @@ const BlogDetail=()=>{
                     <div className="flex flex-col gap-3 pt-10">
                         <h1 className="text-2xl">Write Your Comment</h1>
                         
-                        <div className=" flex max-md:flex-col gap-4 justify-between">
-                            <TextField  sx={{ width:'50%' ,outlineColor:'#755BB4' }} placeholder="your name..."/> 
-                            <TextField sx={{ width:'50%' ,outlineColor:'#755BB4'}} placeholder="email..."/>
+                        <div className=" flex max-sm:flex-col gap-4 justify-between">
+                           <div className=" w-1/2  max-sm:w-[90%] "><TextField sx={{ width:'100%', outlineColor:'red' }}  placeholder="your name..."/> </div> 
+                           <div className="  w-1/2 max-sm:w-[90%]">  <TextField sx={{ width:'100%' }}  placeholder="email..."/></div> 
                         </div>
                         <textarea
                         className="border-2  outline-[#9e88d1]"
@@ -94,10 +105,41 @@ const BlogDetail=()=>{
                           value={commentt} 
                            onChange={(e)=>setComment(e.target.value)}
                           /> 
-                          <button className="w-32 bg-gradient-to-r from-[#755BB4]  to-[#a493ce] text-white text-lg px-5 py-3">Submit</button>
+                          <button className="w-32 bg-gradient-to-r from-[#755BB4]  to-[#a493ce] hover:bg-gradient-to-r hover:from-[#a493ce] hover:to-[#755BB4] text-white text-lg px-5 py-3">Submit</button>
                     </div>
+
+                    <div>
+                     <h1 className="text-2xl font-bold pt-7 pb-4 max-sm:text-center">Related Blog</h1>
+                     <div className="flex  gap-3 max-sm:flex-col  justify-center items-center ">
+                        <Carde2  
+                        datee='october 6,2023'
+                        img={img2}
+                        title='The integration of WhatsApp for order inquiries 
+                        '
+                        />
+
+                        <Carde2 
+                        datee='march 23,2021'
+                        img={img3}
+                        title='The integration of for order inquiries   .
+                        '
+                        />
+                        <Carde2  
+                        datee='june 3,2024'
+                        img={img4}
+                        title='The integration of  the no WhatsApp for order inquiries  .
+                        '
+                        />
+                       
+                    </div>
+                    
+                    </div>
+
+
+
              </div>
-             <div className=" w-2/5 max-lg:w-[100%] ">
+
+             <div className=" w-2/5 max-lg:w-[95%] ">
                <div className="flex">
                 <input 
                 type="text" 
